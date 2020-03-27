b4349f9dd88eb3357678319be5effa9701f9d63b
Claudio Rescigno <cludso@yahoo.it>
2020-03-27 13:36:33 +0100

    creation

---
+++ components/NavBar.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+  <div class="nav">
+    <nuxt-link to="/" class="brand">
+      Real world events
+    </nuxt-link>
+    <nav>
+      <nuxt-link to="/">
+        List
+      </nuxt-link> |
+      <nuxt-link to="/create">
+        Create
+      </nuxt-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
